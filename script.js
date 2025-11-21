6b9e843732b73a3f096d67cb769c7c42769267a5
user.name like "Asif Ekhlaque <asifekhlaque33@gmail.com>
2025-11-22 00:37:45 +0530

    js

--- script.js
+++ script.js
@@ -8,3 +8,27 @@ menuToggle.addEventListener('click', () => {
     navbar.classList.toggle('show');
     menuToggle.querySelector('i').classList.toggle('fa-times');
 });
+
+const carousel = document.querySelector(".carousel");
+const images = document.querySelectorAll(".carousel img");
+let index = 0;
+
+function showSlide() {
+  index++;
+  if (index >= images.length) index = 0;
+  carousel.style.transform = `translateX(${-index * 100}%)`;
+}
+
+let slideInterval = setInterval(showSlide, 3000); // Auto Slide
+
+document.querySelector(".next").addEventListener("click", () => {
+  clearInterval(slideInterval);
+  showSlide();
+});
+
+document.querySelector(".prev").addEventListener("click", () => {
+  clearInterval(slideInterval);
+  index--;
+  if (index < 0) index = images.length - 1;
+  carousel.style.transform = `translateX(${-index * 100}%)`;
+});
